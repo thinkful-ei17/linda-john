60fd74d480e597a847a5e129ea9b21fffe608189
John Castillo <JohnluisCastillo@gmail.com>
2017-12-07 14:28:10 -0500

    index

---
+++ index.js
@@ -0,0 +1,13 @@
+function cat(){
+    $('.thumbnail').on('click', function(event) {
+       const imgSrc = $(this).find('img').attr('src');  
+       const imgAlt = $(this).find('img').attr('alt');
+       $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+      
+    });
+  }
+  
+  $(cat());
+
+
+
