a9f4fadc2f4255788a3a8319ece8543bb7402036
Linda Foo <linda.foo.dev@gmail.com>
2017-12-07 18:15:47 -0800

    created branch to edit for self-review

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-// function cat(){
-//     $('.thumbnail').on('click', function(event) {
-//        const imgSrc = $(this).find('img').attr('src');  
-//        const imgAlt = $(this).find('img').attr('alt');
-//        $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
-      
-//     });
-//   }
-  
-//   $(cat());
+
 
 
 function addItem(){
